49c23739167048493e6d824545e0d580d998a582
Marcus <marcus1sp2@gmail.com>
2022-09-02 17:54:31 +0200

    🔴 testTwoColumns

--- src/MinesweeperTest.java
+++ src/MinesweeperTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class MinesweeperTest {
 
     @Test
-    public void testOneRow() {
+    public void testOneCell() {
         assertArrayEquals(
             new String[]{"0"},
             new Minesweeper(new String[]{"."}).getHints()
@@ -17,6 +17,14 @@ public class MinesweeperTest {
         assertArrayEquals(
                 new String[]{"0", "0"},
                 new Minesweeper(new String[]{".", "."}).getHints()
+        );
+    }
+
+    @Test
+    public void testTwoColumns() {
+        assertArrayEquals(
+                new String[]{"00"},
+                new Minesweeper(new String[]{".."}).getHints()
         );
     }
 
