6c24da002f8ee0f53729f889d8b987a8904e22ef
Marcus <marcus1sp2@gmail.com>
2022-09-02 17:12:52 +0200

    ❤ Initial Commit

---
+++ src/Minesweeper.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Minesweeper {
+
+    @Test
+    public void testTest() {
+        assertTrue(true == true);
+    }
+
+
+}
